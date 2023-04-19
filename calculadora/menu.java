1f6eafbb3caf61b04462173e6e3f8fe797f574c1
Gabriel Hawerroth <gabriel.hawerroth04@gmail.com>
2023-04-19 14:31:46 -0300

    alterado local dos arquivos

---
+++ calculadora/menu.java
@@ -0,0 +1,16 @@
+package calculadora;
+
+public class menu {
+    
+    public void exibirMenu(){
+    System.out.println("");
+    System.out.println("1 - Somar");
+    System.out.println("2 - Subtrair");
+    System.out.println("3 - Multiplicar");
+    System.out.println("4 - Dividir");
+    System.out.println("5 - Ímpar/par");
+    System.out.println("6 - Sair");
+    System.out.print("Digite o número da operação desejada: ");
+    }
+
+}
